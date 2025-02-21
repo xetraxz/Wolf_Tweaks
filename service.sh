b2b11dc13df4931421c28a90175cbaaa25aaf5c9
Xetra <xetrazx@gmail.com>
2025-02-21 14:07:17 +0530

    update service.sh

--- service.sh
+++ service.sh
@@ -1,3 +1,4 @@
+#set governer to conservative
 chmod 777 /sys/devices/system/cpu/cpufreq/scaling_governer
 echo "conservative" > /sys/devices/system/cpu/cpufreq/scaling_governer
 chmod 444 /sys/devices/system/cpu/cpufreq/scaling_governer
